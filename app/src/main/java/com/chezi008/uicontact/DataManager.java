aa33b4537e3d5065215a57dfa9d81d77e6a4311a
sailor <552104693@qq.com>
2019-12-09 15:35:06 +0800

    1.修改头像的显示模式

--- app/src/main/java/com/chezi008/uicontact/DataManager.java
+++ app/src/main/java/com/chezi008/uicontact/DataManager.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 public class DataManager {
 
-    private DataManager(){
+    private DataManager() {
 
     }
 
@@ -20,23 +20,26 @@ public class DataManager {
         return DataManagerHolder.instance;
     }
 
-    static class DataManagerHolder{
+    static class DataManagerHolder {
         private final static DataManager instance = new DataManager();
     }
+
     String imgs[] = new String[]{
-            "http://img5.duitang.com/uploads/item/201507/05/20150705000710_zwPH5.thumb.700_0.jpeg",
-            "http://b-ssl.duitang.com/uploads/item/201706/22/20170622131955_h4eZS.thumb.700_0.jpeg",
-            "http://5b0988e595225.cdn.sohucs.com/images/20171030/26ed195281334ba4b1752394b60eb29a.jpeg",
-            "http://img.52z.com/upload/news/image/20180411/20180411111738_20831.jpg",
-            "http://img.52z.com/upload/news/image/20181011/20181011092002_79347.jpg"
+            "https://pqcloud-data.oss-cn-hangzhou.aliyuncs.com/0/dp/type_images/103042_1.png",
+            "http://b-ssl.duitang.com/uploads/item/201410/09/20141009224754_AswrQ.jpeg",
+            "http://pic1.zhimg.com/50/v2-6444e641d0235006e81bc4210b5da89b_hd.jpg",
+            "http://pic3.zhimg.com/50/v2-3f343ce2639268c29a21c1fb01bdc2a2_hd.jpg",
+            "https://fc3tn.baidu.com/it/u=3025909226,1763324618&fm=202&src=bqdata",
+            "http://uploadfile.bizhizu.cn/2015/0514/20150514023641335.jpg"
     };
-    public List<ContactBean> getTestData(String[] array ){
+
+    public List<ContactBean> getTestData(String[] array) {
         List<ContactBean> mData = new ArrayList<>();
         //模拟一些数据
         for (int i = 0; i < 20; i++) {
             ContactBean contactBean = new ContactBean();
-            contactBean.setName(array[i%12]);
-            contactBean.setAvatar(imgs[i%5]);
+            contactBean.setName(array[i % 12]);
+            contactBean.setAvatar(imgs[i % 6]);
             mData.add(contactBean);
         }
         //添加三个头部
@@ -44,16 +47,16 @@ public class DataManager {
         contactBean.setName("头部1");
         contactBean.setIndex(1);
         contactBean.setNum(10);
-        contactBean.setAvatar("intres"+R.drawable.ic_default_avatar);
+        contactBean.setAvatar("intres" + R.drawable.ic_default_avatar);
         mData.add(contactBean);
         ContactBean contactBean2 = new ContactBean();
         contactBean2.setName("z头部2");
-        contactBean2.setAvatar("intres"+R.mipmap.ic_friend);
+        contactBean2.setAvatar("intres" + R.mipmap.ic_friend);
         contactBean2.setIndex(2);
         mData.add(contactBean2);
         ContactBean contactBean3 = new ContactBean();
         contactBean3.setName("y头部3");
-        contactBean3.setAvatar("intres"+R.mipmap.ic_group_avatar);
+        contactBean3.setAvatar("intres" + R.mipmap.ic_group_avatar);
         contactBean3.setIndex(3);
         mData.add(contactBean3);
 
